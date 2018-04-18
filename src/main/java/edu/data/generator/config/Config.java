aeecb601a3db47984b3d016325e75c034cb55ec8
Szabo Zoltan <szabozoltanbors@gmail.com>
2018-04-18 20:07:44 +0300

    Config set for the colaboratory tutorial

--- src/main/java/edu/data/generator/config/Config.java
+++ src/main/java/edu/data/generator/config/Config.java
@@ -14,13 +14,13 @@ public interface Config {
     Integer IMAGE_HEIGHT = 416;
     Integer ROWS = 2;
     Integer COLS = 2;
-    Integer DATA_SET_SIZE = 5;
+    Integer DATA_SET_SIZE = 5000;
     Integer VAL_SET_SIZE = 5;
     Integer MARGIN_AROUND = 10; // 10px space around the playground
     Integer MARGIN_BETWEEN = 10; // 10px space between the cards
     Integer MAX_ANGLE_TO_ROTATE = 30;
     boolean BLUR = true;
-    boolean DEBUG = true;
+    boolean DEBUG = false;
     boolean DARKNET = true; // Generates labels and train-val.txt for darknet
     String[] CLASS_LABELS = {"winnie-the-pooh", "tiger"};
 }
